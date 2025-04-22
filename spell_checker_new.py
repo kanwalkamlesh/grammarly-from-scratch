d5c4e351d121bea4fc818181b285df3973f8e972
kanwalkamlesh <159791431+kanwalkamlesh@users.noreply.github.com>
2025-04-23 04:09:04 +0530

    Update spell_checker_new.py

--- spell_checker_new.py
+++ spell_checker_new.py
@@ -1,21 +1,14 @@
 from spellchecker import SpellChecker
 
-# Initialize the spell checker
-spell = SpellChecker()
-
 def check_spelling(text):
-    # Tokenize the input text into words
-    words = text.split()
+    spell = SpellChecker()
 
-    # Find words that might be misspelled
+    words = text.split()
     misspelled = spell.unknown(words)
 
-    # Store the suggestions for each misspelled word
-    suggestions = {}
-
+    corrections = {}
     for word in misspelled:
-        # Get the most likely correction for the misspelled word
-        suggestions[word] = spell.correction(word)
+        correction = spell.correction(word)
+        corrections[word] = correction
 
-    return suggestions
-  # Import the spell check module
+    return corrections
